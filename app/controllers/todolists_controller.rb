74cb5d26b3b1451e9cd694899342d4011ba3dab7
yamoritaketo <ymymz87@icloud.com>
2021-02-03 00:26:31 +0900

    [add]google_api

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,7 +5,12 @@ class TodolistsController < ApplicationController
 
   def create
     list = List.new(list_params)
+    list.score = Language.get_data(list_params[:body])
     list.save
+    tags = Vision.get_image_data(list.image)
+    tags.each do |tag|
+      list.tags.create(name: tag)
+    end
     redirect_to todolist_path(list.id)
   end
 
